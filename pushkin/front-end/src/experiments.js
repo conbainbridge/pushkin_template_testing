6a75551c28123aa2816c7b162cc219db58649ba6
Connie <37002158+conbainbridge@users.noreply.github.com>
2020-06-03 11:11:04 -0400

    logo file update to use existing image in src/assets/images/quiz

--- pushkin/front-end/src/experiments.js
+++ pushkin/front-end/src/experiments.js
@@ -1,4 +1,4 @@
 import pushkinComponentaa3feec69c2c4c31a02e642e50e153d3 from 'pushkinComponentaa3feec69c2c4c31a02e642e50e153d3';
 export default [
-	{ fullName: 'myexp Experiment', shortName: 'myexp', module: pushkinComponentaa3feec69c2c4c31a02e642e50e153d3, logo: 'logo512.png', tagline: 'Be a citizen scientist! Try this quiz.', duration: '' },
+	{ fullName: 'myexp Experiment', shortName: 'myexp', module: pushkinComponentaa3feec69c2c4c31a02e642e50e153d3, logo: 'Mind.png', tagline: 'Be a citizen scientist! Try this quiz.', duration: '' },
 ];
